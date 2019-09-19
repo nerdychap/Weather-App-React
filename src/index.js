d3a8f54a0dce17bfdd8f05286d5408c7bda050de
nerdychap <nerdychap@gmail.com>
2019-09-19 15:16:54 +0200

    removing constructor binding and making location an arrow function

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ class Car extends React.Component {
 				aveTemp: 0,
 				maxTemp: 0
 			};
-			this.location = this.location.bind(this);
+			//this.location = this.location.bind(this);
 		}
-		location() {
+		location = () => {
 			if (navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition((position) => {
 				var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude.toPrecision(4) + '&lon=' + position.coords.longitude.toPrecision(4)  + '&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9';
