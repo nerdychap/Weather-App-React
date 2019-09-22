8dd5f517686b51173d80ff021a5361298e869c40
nerdychap <nerdychap@gmail.com>
2019-09-23 00:09:42 +0200

    testing file upload

--- src/index.js
+++ src/index.js
@@ -79,6 +79,7 @@ class Car extends React.Component {
 			<input type="submit" value="Enter"/>
 			</form>
 			<div><img src={this.state.img}/></div>
+			<input type="file" accept="image/*" capture="camera" />
 			<div>{this.state.city}{this.state.country}</div>
 			<div>{this.state.description}</div>
 			<div>Min: {this.state.minTemp} °C</div> 
