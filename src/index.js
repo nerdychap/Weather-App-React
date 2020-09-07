fe2bbe0d5b0197c5e238643e479151d72c68684f
siyabonga <siyabonga.hlengwa@mirumagency.com>
2020-09-07 14:14:35 +0200

    code refactoring and app improvement

--- src/index.js
+++ src/index.js
@@ -1,91 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from 'react';
 import './style.scss';
+import { render } from 'react-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 
-class Car extends React.Component {
-		constructor(props){
-			super(props);
-			this.state = {
-				locus: "",
-				city: "",
-				country: "",
-				img: "",
-				description: "",
-				minTemp: 0,
-				aveTemp: 0,
-				maxTemp: 0
-			};
-		}
-		location = () => {
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition((position) => {
-				var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude.toPrecision(4) + '&lon=' + position.coords.longitude.toPrecision(4)  + '&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9';
-				var myHeaders = new Headers();
-				var myInit = { method: 'GET',
-					headers: myHeaders,
-					mode: 'cors',
-					cache: 'default' };	
-				fetch(url, myInit).then(function(resp){
-					return resp.json();
-				}).then((ans)=>{ 
-				this.setState({
-					city: ans.name + ', ',
-					country: ans.sys.country,
-					img: 'https://openweathermap.org/img/wn/' + ans.weather[0].icon + '@2x.png',
-					description: ans.weather[0].description,
-					minTemp: (ans.main.temp_min).toPrecision(2),
-					aveTemp: (ans.main.temp).toPrecision(2),
-					maxTemp: (ans.main.temp_max).toPrecision(2)
+const App = () => {
+	const [location, setLocation] = useState("Country");
+	const [forecast, setForecast] = useState({});
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [temps, setTemps] = useState({});
+	const [country, setCountry] = useState("");
+	const [weather, setWeather] = useState({});
+	const [error, setError] = useState();
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				var url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude.toPrecision(4) + '&lon=' + position.coords.longitude.toPrecision(4) + '&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9';
+				fetchData(url).then(data => {
+					setIsLoaded(true);
+					setForecast(data);
+					setTemps(data.main);
+					setCountry(data.sys.country);
+					setWeather(data.weather[0])
+				}).catch(error => {
+					setError(error)
 				});
 			});
-				});
-			} else {
-				document.getElementById("message").innerHTML = "Geolocationis not supported by this browser";
-			}
-		};
-		enteredCity = (event) => {
-			event.preventDefault();
-			var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + this.state.locus + '&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9';
-			var myHeaders = new Headers();
-		    var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' };	
-			fetch(url, myInit).then(function(resp){
-			return resp.json();
-			}).then(function(ans){ 
-				this.setState({
-					city: ans.name + ', ',
-					country: ans.sys.country,
-					img: 'https://openweathermap.org/img/wn/' + ans.weather[0].icon + '@2x.png',
-					description: ans.weather[0].description,
-					minTemp: (ans.main.temp_min).toPrecision(2),
-					aveTemp: (ans.main.temp).toPrecision(2),
-					maxTemp: (ans.main.temp_max).toPrecision(2)
-				});
-			}.bind(this));		
-		}; 
-		stater = (event) => {
-			this.setState({locus: event.target.value});
+		} else {
+			setError("Geolocation is not supported by this browser");
 		}
-	render(){
-		var img = "";
-		return (
-		<div>
-			<button onClick={this.location}>Local Weather</button>
-			<div id="message"></div>
-			<form onSubmit={this.enteredCity} netlify>
-			<input type="text" onChange={this.stater} placeholder="Location"/>
-			<input type="submit" value="Enter"/>
-			</form>
-			<div><img src={this.state.img} alt="" /></div>
-			<div>{this.state.city}{this.state.country}</div>
-			<div>{this.state.description}</div>
-			<div>Min: {this.state.minTemp} °C</div> 
-			<div>Actual: {this.state.aveTemp} °C</div> 
-			<div>Max: {this.state.maxTemp} °C</div> 
-		</div>
-		);
+	}, []);
+
+	const getWeather = () => {
+		setIsLoaded(false)
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=75500792ea7488c01a878f05c8adf1d9`;
+		fetchData(url)
+			.then(data => {
+				setIsLoaded(true);
+				setForecast(data);
+				setTemps(data.main);
+				setCountry(data.sys.country);
+				setWeather(data.weather[0])
+			}).catch(error => {
+				setError(error)
+				console.log(error.message);
+			});
 	}
+
+
+	if (!isLoaded) {
+		return <h1>Loading...</h1>
+	}
+	else if (error) {
+		return <h1>{error}</h1>
+	}
+	const { description, icon } = weather;
+	const { temp, temp_max, temp_min } = temps;
+	const { name } = forecast;
+	return (
+
+		<div className="container">
+			<h1>Weather Forecast</h1>
+			<div className="form">
+				<label htmlFor="location">Enter Location: <input type="text" name="location" id="location" onChange={(e) => { setLocation(e.target.value) }} /></label>
+				<button onClick={getWeather}>Enter </button>
+			</div>
+			<section>
+				<div>{name}, {country}</div>
+				<div>{description}</div>
+				<div><img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt={`${description} icon`} /></div>
+				<div className="temps">
+					<span>{temp_min}</span>
+					<div>{temp}</div>
+					<span>{temp_max}</span>
+				</div>
+				<h1>{error}</h1>
+			</section>
+		</div>
+
+
+	)
 }
-ReactDOM.render(<Car/>, document.getElementById('root'));
+
+const fetchData = (url) => {
+	var myHeaders = new Headers();
+	var myInit = {
+		method: 'GET',
+		headers: myHeaders,
+		mode: 'cors',
+		cache: 'default'
+	};
+	return fetch(url, myInit).then((res) => res.json())
+}
+
+render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'))
